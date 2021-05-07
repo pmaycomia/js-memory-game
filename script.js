4850604c61640dc771a0067ec7392fd7822934bc
pmaycomia <pmaycomia@gmail.com>
2021-05-07 22:16:53 +0200

    Rename files

---
+++ script.js
@@ -0,0 +1,74 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+const cardsArray = [
+    {
+        name: 'shield',
+        img: 'images/shield.jpg'
+    },
+    {
+        name: 'navi',
+        img: 'images/navi.jpg'
+    },
+    {
+        name: 'rupee',
+        img: 'images/rupee.jpg'
+    },
+    {
+        name: 'newheart',
+        img: 'images/newheart.jpg'
+    },
+    {
+        name: 'dekunuts',
+        img: 'images/dekunuts.jpg'
+    },
+    {
+        name: 'slingshot',
+        img: 'images/slingshot.jpg'
+    },
+    {
+        name: 'bomb',
+        img: 'images/bomb.jpg'
+    },
+    {
+        name: 'potion',
+        img: 'images/potion.jpg'
+    },
+    {
+        name: 'boomerang',
+        img: 'images/boomerang.jpg'
+    },
+    {
+        name: 'bombchu',
+        img: 'images/bombchu.jpg'
+    },
+    {
+        name: 'gem',
+        img: 'images/gem.jpg'
+    },
+    {
+        name: 'heal',
+        img: 'images/heal.jpg'
+    },
+    {
+        name: 'bombchu',
+        img: 'images/bombchu.jpg'
+    },
+    {
+        name: 'blank',
+        img: 'images/blank.jpg'
+    },
+    {
+        name: 'magicbean',
+        img: 'images/magicbean.jpg'
+    },
+    {
+        name: 'halfheart',
+        img: 'images/halfheart.jpg'
+    }
+]
+
+
+
+
+
+})
